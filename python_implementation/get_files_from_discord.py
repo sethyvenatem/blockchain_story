92f74b0bc44235813d67fdf02d33e2a0d105db9b
Steven Mathey <steven.mathey@cleveron.ch>
2023-09-18 23:10:51 +0200

    discord bot to get all files

---
+++ python_implementation/get_files_from_discord.py
@@ -0,0 +1,29 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+guild = discord.Guild
+token = 'MTE1MzI5NTkxMDg5MDEzMTU1Nw.Guu-v9.uss6alnVVMgst7Coaqx5cnPjwRTm93AEgdAqXE'
+signed_chapters_id = 1133791244858966096
+validates_stories_id = 1133791437637558332
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(signed_chapters_id)
+    messages = channel.history(limit=1000)#.flatten()
+    async for mes in messages:
+        for file in mes.attachments:
+            if file.filename.endswith('.json'):
+                await file.save('test_'+file.filename)
+                
+    channel = client.get_channel(validates_stories_id)
+    messages = channel.history(limit=1000)#.flatten()
+    async for mes in messages:
+        for file in mes.attachments:
+            if file.filename.endswith('.json'):
+                await file.save('test_'+file.filename)
+                
+    await client.close()
+
+client.run(token)
