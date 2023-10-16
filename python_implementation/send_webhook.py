145ac734d4d88aa6382c514c05c512089c1b9d79
Steven Mathey <steven.mathey@cleveron.ch>
2023-10-16 23:23:41 +0200

    intro webhook

--- python_implementation/send_webhook.py
+++ python_implementation/send_webhook.py
@@ -24,28 +24,60 @@ webhook.username = 'sethyvenatem'
 webhook.execute()
 
 webhook = DiscordWebhook(url=webhook_url)
+webhook.content = text[1]
 webhook.username = 'sethyvenatem'
+
 embed = DiscordEmbed()
 #embed.set_author('sethyvenatem')
 #Set the title and description of the embed
-embed.set_title(text[1])
-embed.set_description(text[2])
+embed.set_title(text[2])
+embed.set_description(text[3])
 #Add the embed to the webhook
 webhook.add_embed(embed)
 
 embed = DiscordEmbed()
 #embed.set_author('sethyvenatem')
 #Set the title and description of the embed
-embed.set_title(text[3])
-embed.set_description(text[4])
+embed.set_title(text[4])
+embed.set_description(text[5])
 #Add the embed to the webhook
 webhook.add_embed(embed)
 
 embed = DiscordEmbed()
 #embed.set_author('sethyvenatem')
 #Set the title and description of the embed
-embed.set_title(text[5])
-embed.set_description(text[6])
+embed.set_title(text[6])
+embed.set_description(text[7])
+#Add the embed to the webhook
+webhook.add_embed(embed)
+
+embed = DiscordEmbed()
+#embed.set_author('sethyvenatem')
+#Set the title and description of the embed
+embed.set_title(text[8])
+embed.set_description(text[9])
+#Add the embed to the webhook
+webhook.add_embed(embed)
+
+embed = DiscordEmbed()
+#embed.set_author('sethyvenatem')
+#Set the title and description of the embed
+embed.set_title(text[10])
+embed.set_description(text[11])
+#Add the embed to the webhook
+webhook.add_embed(embed)
+
+webhook.execute()
+
+webhook = DiscordWebhook(url=webhook_url)
+webhook.content = text[12]
+webhook.username = 'sethyvenatem'
+
+embed = DiscordEmbed()
+#embed.set_author('sethyvenatem')
+#Set the title and description of the embed
+embed.set_title(text[13])
+embed.set_description(text[14])
 #Add the embed to the webhook
 webhook.add_embed(embed)
 
